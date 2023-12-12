ce07301fb33481b13ccddec87adca4aadb7d8d2d
Aaron Imbrock <aimbrock@gmail.com>
2023-12-12 17:05:43 -0600

    basics/14

---
+++ atourofgo/type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42	// change me!
+	j := '4'
+	x := 'x'
+	fmt.Printf("v is of type %T.\n", v)
+	fmt.Printf("j is of type %T.\n", j)
+	fmt.Printf("x is of type %T.\n", x)
+}
