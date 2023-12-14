d8b70bc94015000b2f59495027b2a5e300b3d732
Aaron Imbrock <aimbrock@gmail.com>
2023-12-14 11:41:34 -0600

    square root implementation works well enough

--- atourofgo/flow_control/exercise-loops-and-functions.go
+++ atourofgo/flow_control/exercise-loops-and-functions.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func oSqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
 func Sqrt(x float64) float64 {
-	guess := 
+	z := float64(1)
+	n := 0
+	for i := 0; i < 100; i++ {
+		z -= (z*z - x) / (2 * z)
+		if (z*z > x) {
+			z -= 0.00001
+		} 
+	}
+	fmt.Printf("We took %v times to guess %v\n", n, z)
+	return z
 }
 
 func main() {
-	fmt.Println(sqrt(2))
+	fmt.Println("My Sqrt is: ", Sqrt(2))
+	fmt.Println("Go's Sqrt is: ", oSqrt(2))
 }
-
